a3fc82c681b27f4457902ac973c5273cc4724773
Lana Lebedeva <batetnikova@mail.ru>
2019-11-23 11:54:33 +0300

    перед выгрузкой на сайт

--- card.c
+++ card.c
@@ -17,6 +17,11 @@ int main()
 			else if (card_name[0]=='A')	
 				val=11;
 				else val=atoi(card_name);
+	/* Check if the value is between 3 and 6 */
+	if ((val>2) && (val<=7))
+		puts ("Counter increased");
+	else if (val == 10)
+		puts ("Counter decreased");
 	
 	printf ("value cards: %i\n", val);
 	system ("pause>nul");
